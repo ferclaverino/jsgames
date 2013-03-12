eebf55fc8726ef3cf1f861623a2ed67a26c2caea
ferclaverino <fernando.claverino@gmail.com>
2013-03-12 18:11:29 +0100

    planet moving

---
+++ utopia/accelerator.js
@@ -0,0 +1,47 @@
+function Accelerator() {
+	var self = this;
+  var state = "stop";
+  var velocity = 0;
+  var aceleration = 0.02;
+  var maxVelocity = 0.5;
+  
+  this.push = function () {
+    state = "starting";
+    maxVelocity += 0.1;
+  }
+  
+  this.update = function () {
+    if (state == "starting") {
+      self.accelerate();
+    } else if (state == "stoping") {
+      self.deAccelerate();
+    }
+  }
+  
+  this.getVelocity = function () {
+    return velocity;
+  }
+  
+  self.accelerate = function () {
+    if (velocity >= maxVelocity) {
+      state = "stoping";
+      velocity = maxVelocity;
+    } else if (velocity < maxVelocity) {
+      velocity += aceleration;
+    }
+  }
+  
+  self.deAccelerate = function () {
+    if (velocity <= 0) {
+      state = "stop";
+      velocity = 0;
+    } else if (velocity > 0) {
+      velocity -= aceleration * 2;
+    }
+    self.resetMaxVelocity();
+  }
+  
+  self.resetMaxVelocity = function () {
+    maxVelocity = 0.5;
+  }
+}
---
+++ utopia/planet.js
@@ -0,0 +1,61 @@
+function Planet(accelerator) {
+	var self = this;
+	var PI = 3.1415;
+  var rotateInRadians = 2 * PI;
+  var width = 712;
+  var height  = 717;
+  self.accelerator = accelerator;
+  
+	this.init = function(canvas) {
+    canvas.addEventListener("click", click , false);
+	}
+	
+  function click(e) {
+    self.accelerator.push();
+  }
+  
+	this.visible = function() {
+		return true;
+	}
+  
+	this.update = function(delta) {
+		self.accelerator.update();
+    
+    var velocity = self.accelerator.getVelocity();
+    if (velocity > 0) {
+      rotateInRadians -= delta * velocity;
+      
+      if (rotateInRadians < 0) {
+        rotateInRadians = 2 * PI;
+      }
+    }
+	}
+	
+	this.draw = function(context) {
+		var image = new Image();
+		image.src = "planet.png";
+    
+    // save the context's co-ordinate system before 
+    // we screw with it
+    context.save(); 
+    
+    // move the origin to 50, 35   
+    //context.translate((context.width-width) / 2, 300); 
+    context.translate((context.canvas.width - width) / 2, context.canvas.height / 2); 
+    
+    // now move across and down half the 
+    // width and height of the image (which is 128 x 128)
+    context.translate(width/2, height /2); 
+    
+    // rotate around this point
+    context.rotate(rotateInRadians); 
+    
+    // then draw the image back and up
+    context.drawImage(image, width/-2, height /-2); 
+    
+    // and restore the co-ordinate system to its default
+    // top left origin with no rotation
+    context.restore();
+    
+	}
+}
